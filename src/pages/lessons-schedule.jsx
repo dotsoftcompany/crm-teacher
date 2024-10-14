0fbfd796739e6a4f8c35e5ef3d411d99eae3cfad
Mukhammadxuja Anvarov <87266381+mukhammadxuja@users.noreply.github.com>
2024-10-14 13:12:38 +0500

    LessonsSchedule mobile

--- src/pages/lessons-schedule.jsx
+++ src/pages/lessons-schedule.jsx
@@ -1,6 +1,6 @@
 import BreadcrumbComponent from '@/components/breadcrumb';
 import { useMainContext } from '@/context/main-context';
-import { ChevronRight } from 'lucide-react';
+import { ChevronRight, Clock } from 'lucide-react';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
@@ -57,49 +57,54 @@ function LessonsSchedule() {
         </p>
       </div>
 
-      <div className="grid grid-cols-8 gap-x-1.5">
-        <div></div>
-        {days.map((day) => (
-          <div key={day} className="font-semibold text-center p-2 pb-4">
-            {day}
-          </div>
-        ))}
-
-        {/* Time slots and schedule cells */}
-        {times.map((time, timeIndex) => (
-          <React.Fragment key={time}>
-            {/* Time column */}
-            <div className="font-semibold text-center bg-accent dark:bg-background border dark:border-none rounded-md p-2">
-              {time}
+      <div className="overflow-x-auto">
+        <div className="grid grid-cols-8 gap-x-1 md:gap-x-1.5 min-w-[50rem] w-full">
+          <div></div>
+          {days.map((day) => (
+            <div key={day} className="font-semibold text-center p-2 pb-4">
+              {day}
             </div>
-            {/* Cells for each day */}
-            {days.map((day) => {
-              // Find if there's a class scheduled for this time and day
-              const groupForDay = groups.find((schedule) => {
-                const scheduleDays = getDaysForSchedule(schedule.selectedDay);
-                return (
-                  scheduleDays.includes(day) &&
-                  isTimeInRange(time, schedule.timeInDay)
-                );
-              });
+          ))}
 
-              if (groupForDay) {
-                const isFirstTimeSlot =
-                  timeIndex ===
-                  times.indexOf(groupForDay.timeInDay.split(' - ')[0]);
+          {/* Time slots and schedule cells */}
+          {times.map((time, timeIndex) => (
+            <React.Fragment key={time}>
+              {/* Time column */}
+              <div className="sticky left-0 z-10 flex items-center justify-center gap-1 md:gap-2 font-semibold text-center dark:bg-background p-2 bg-background">
+                <Clock className="w-4 h-4" />
+                <span>{time}</span>
+              </div>
+              {/* Cells for each day */}
+              {days.map((day) => {
+                // Find if there's a class scheduled for this time and day
+                const groupForDay = groups.find((schedule) => {
+                  const scheduleDays = getDaysForSchedule(schedule.selectedDay);
+                  return (
+                    scheduleDays.includes(day) &&
+                    isTimeInRange(time, schedule.timeInDay)
+                  );
+                });
 
-                return (
-                  <Link to={`/groups/${groupForDay.id}`}>
-                    <div
-                      key={day + time}
-                      className={`p-2 h-10 text-center font-semibold cursor-pointer ${getBackgroundColor(
-                        groupForDay.selectedDay
-                      )} ${isFirstTimeSlot ? 'rounded-t-md' : 'rounded-b-md'}`}
-                    >
-                      {isFirstTimeSlot ? (
-                        <>
-                          <div className="flex items-center justify-between rounded-t-md group">
-                            <div className="flex items-center gap-1">
+                if (groupForDay) {
+                  const isFirstTimeSlot =
+                    timeIndex ===
+                    times.indexOf(groupForDay.timeInDay.split(' - ')[0]);
+
+                  return (
+                    <Link to={`/groups/${groupForDay.id}`}>
+                      <div
+                        key={day + time}
+                        className={`p-2 h-10 text-center font-semibold cursor-pointer ${getBackgroundColor(
+                          groupForDay.selectedDay
+                        )} ${
+                          isFirstTimeSlot
+                            ? 'rounded-t-sm md:rounded-t-md'
+                            : 'rounded-b-sm md:rounded-b-md'
+                        }`}
+                      >
+                        {isFirstTimeSlot ? (
+                          <>
+                            <div className="flex flex-wrap items-center gap-1 rounded-t-sm md:rounded-t-md group">
                               <p className="text-sm">
                                 {
                                   courses.filter(
@@ -110,28 +115,28 @@ function LessonsSchedule() {
                               <small className="opacity-80">
                                 #{groupForDay.groupNumber}
                               </small>
+                              <ChevronRight className="w-4 h-4 group-hover:ml-1 duration-200" />
                             </div>
-                            <ChevronRight className="w-4 h-4 group-hover:mr-1 duration-200" />
-                          </div>
-                        </>
-                      ) : (
-                        <div></div>
-                      )}
-                    </div>
-                  </Link>
-                );
-              }
+                          </>
+                        ) : (
+                          <div></div>
+                        )}
+                      </div>
+                    </Link>
+                  );
+                }
 
-              // Render empty cell if no class is scheduled
-              return (
-                <div
-                  key={day + time}
-                  className="border rounded-md p-2 h-10"
-                ></div>
-              );
-            })}
-          </React.Fragment>
-        ))}
+                // Render empty cell if no class is scheduled
+                return (
+                  <div
+                    key={day + time}
+                    className="border rounded-md p-2 h-10"
+                  ></div>
+                );
+              })}
+            </React.Fragment>
+          ))}
+        </div>
       </div>
     </div>
   );
